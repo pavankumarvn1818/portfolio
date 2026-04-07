3600706c4459756fd6a5d659d4280d6d2dd259ae
Pavan Kumar VN <117066729+pavankumarvn1818@users.noreply.github.com>
2026-04-07 17:11:30 +0530

    server link  added

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -25,7 +25,7 @@ export default function Contact() {
     setStatus("Sending...");
 
     try {
-      const res = await fetch("http://localhost:5000/contact", {
+      const res = await fetch("https://portfolio-iapg.onrender.com", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
